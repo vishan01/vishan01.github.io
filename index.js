d865cb5719175134843b8d43a182ccbec59f1645
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2024-03-03 16:05:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 300) {
-      $('#skill-con').addClass('animated bounceOutLeft'); 
-    } 
-   });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      }
+      else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden-left');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElement = document.querySelectorAll('.hidden-right');
+hiddenElement.forEach((el) => observer.observe(el));
